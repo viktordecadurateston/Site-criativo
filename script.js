471cb6f0330bd92762acb49c5334a88e34a22271
viktordecadurateston <151945840+viktordecadurateston@users.noreply.github.com>
2024-04-19 09:25:31 -0300

    site3

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ function mostrarDescricao(cuboId) {
     descricaoDiv.style.display = 'block';
     
     if (cuboId === 'cubo1') {
-        descricaoDiv.innerHTML = '<h2>Descrição do Cubo 1</h2><p>Esta é a descrição do Cubo 1.</p>';
+        descricaoDiv.innerHTML = '<h2>Descrição do Cubo 1</h2><p>Esta é a descrição do Cubo 2x2.</p>';
     } else if (cuboId === 'cubo2') {
-        descricaoDiv.innerHTML = '<h2>Descrição do Cubo 2</h2><p>Esta é a descrição do Cubo 2.</p>';
+        descricaoDiv.innerHTML = '<h2>Descrição do Cubo 2</h2><p>Esta é a descrição do Cubo 3x3.</p>';
     }
+    // Adicione mais blocos else if conforme necessário para mais cubos
 }
